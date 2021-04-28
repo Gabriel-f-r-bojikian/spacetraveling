64caeac747887ad839638c13b338c725609fea98
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2021-04-28 20:40:31 -0300

    Revert "Header logo brings users back to the homepage"
    Several hanges were made to the index.tsx file that created strange
    behaviours and the best choice was to revert back to the last working
    stage.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,12 @@
 import Home from "../../pages";
 import styles from "./header.module.scss";
-import Link from 'next/link';
 
 export default function Header() {
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Link href="/">
-          <a>
-          <img src="assets/Vectorlogo.svg" alt="logo"/>
-          <img src="assets/spacetravelingLogoName.svg" alt="Space Traveling"/>
-          </a>
-        </Link>
+        <img src="assets/Vectorlogo.svg" alt="logo"/>
+        <img src="assets/spacetravelingLogoName.svg" alt="Space Traveling"/>
       </div>
     </header>
   )
